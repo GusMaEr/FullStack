60b4f1ada3ff947732c7603335af8c09818c5870
Gustavo ERNESTO <gmernesto@hotmail.com>
2022-05-29 16:57:59 -0300

    20220529_JS

--- JavaScript/listadoAlumnos/listAlu.js
+++ JavaScript/listadoAlumnos/listAlu.js
@@ -1,29 +1,53 @@
-document.addEventListener("keypress", function(e){
-// console.log(e.key)
-if (e.key=="Enter") {
-    let inputs=document.getElementsByTagName("input")
-    for (let i = 0; i < input.length; i++) {
-        inputs[i].style.display="nome"
-    }     
- }  
- }
+/**
+ * 
+ */
+ document.addEventListener("keypress", function(e) {
+    // console.log(e.key)
+    if (e.key=="Enter") {
+        let inputs=document.getElementsByTagName("input")
+        // console.log(inputs)  
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].style.display="none"
+        }          
+    }
+}
 )
 
-function showInput(id){
-document.getElementById(id).style.display="block"
-document.getElementById(id).focus
+
+/**
+ * 
+ * @param {*} id 
+ */
+function showInput(id) {
+    document.getElementById(id).style.display="block"
+    document.getElementById(id).focus()
 }
 
-function updTexto(id,texto){
-document.getElementById(id).innerHTML=texto
+/**
+ * Modifica el texto de la tabla según lo
+ * que se ingresó en el input
+ * @param {*} id 
+ * @param {*} texto 
+ */
+function updTexto(id, texto) {
+    document.getElementById(id).innerHTML=texto
+}
+
+/**
+ * Pone visible todos los inputs de la fila
+ * dada por el parámetro fila
+ * @param {*} fila recibe el número de la fila
+ */
+function showAllInputs(fila) {
+    // console.log(document.getElementById("txtnom1").innerHTML)
+
+    // console.log(document.querySelector("input#dni1"))
+    document.querySelector(`input#nom${fila}`).style.display="block"
+    document.querySelector(`input#ape${fila}`).style.display="block"
+    document.querySelector(`input#dni${fila}`).style.display="block"
 
 }
-function showAllInput(fila){
-    document.querySelector("input#nom$").style.display="block"
-    document.querySelector('inpunt#nom$(fila)').style.display="block"
-}
 
-function ocualtarFila(id){
+function ocultarFila(id) {
     document.getElementById(id).style.display="none"
 }
-
